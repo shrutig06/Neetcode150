5c743d8b497e23ba9688af8102def6362246853d
Shruti Gupta <shrutigupta72552@gmail.com>
2026-06-19 00:03:32 +0530

    Time: 28 ms (48.61%) | Memory: 18.3 MB (39.36%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -3,7 +3,7 @@ public:
     queue<pair<int, int>> q;
     vector<int> move = {-1, 0, 1, 0};
 
-    void runBFS(const vector<vector<char>>& grid, vector<vector<bool>>& vis) {
+    void runBFS(vector<vector<char>>& grid) {
         int rows = grid.size();
         int cols = grid[0].size();
         while (!q.empty()) {
@@ -12,10 +12,9 @@ public:
             q.pop();
             for (int i = 0; i <= 3; i++) {
                 int newX = x + move[i], newY = y + move[3 - i];
-                if (newX >= 0 && newY >= 0 && newX < rows && newY < cols &&
-                    !vis[newX][newY] && grid[newX][newY] == '1') {
+                if (newX >= 0 && newY >= 0 && newX < rows && newY < cols && grid[newX][newY] == '1') {
                     q.push({newX, newY});
-                    vis[newX][newY] = true;
+                    grid[newX][newY]='0';
                 }
             }
         }
@@ -23,16 +22,15 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int rows = grid.size();
         int cols = grid[0].size();
-        vector<vector<bool>> vis(rows, vector<bool>(cols, false));
         int cnt = 0;
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                if (!vis[i][j] && grid[i][j] == '1') {
+                if (grid[i][j] == '1') {
                     q.push({i, j});
-                    vis[i][j] = true;
+                    grid[i][j]='0';
                     cnt++;
-                    runBFS(grid, vis);
+                    runBFS(grid);
                 }
             }
         }
