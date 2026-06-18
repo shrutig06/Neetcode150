a69c88528874a72c6e091823825f54b244a9cdee
Shruti Gupta <shrutigupta72552@gmail.com>
2026-06-19 00:22:44 +0530

    Time: 24 ms (77.60%) | Memory: 18.1 MB (40.10%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -6,6 +6,18 @@ public:
     void runBFS(vector<vector<char>>& grid) {
         int rows = grid.size();
         int cols = grid[0].size();
+        while (!q.empty()) {
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            for (int i = 0; i <= 3; i++) {
+                int newX = x + move[i], newY = y + move[3 - i];
+                if (newX >= 0 && newY >= 0 && newX < rows && newY < cols && grid[newX][newY] == '1') {
+                    q.push({newX, newY});
+                    grid[newX][newY]='0';
+                }
+            }
+        }
     }
     int numIslands(vector<vector<char>>& grid) {
         int rows = grid.size();
@@ -16,22 +28,9 @@ public:
             for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == '1') {
                     q.push({i, j});
-                    grid[i][j] = '0';
+                    grid[i][j]='0';
                     cnt++;
-
-                    while (!q.empty()) {
-                        int x = q.front().first;
-                        int y = q.front().second;
-                        q.pop();
-                        for (int i = 0; i <= 3; i++) {
-                            int newX = x + move[i], newY = y + move[3 - i];
-                            if (newX >= 0 && newY >= 0 && newX < rows &&
-                                newY < cols && grid[newX][newY] == '1') {
-                                q.push({newX, newY});
-                                grid[newX][newY] = '0';
-                            }
-                        }
-                    }
+                    runBFS(grid);
                 }
             }
         }
