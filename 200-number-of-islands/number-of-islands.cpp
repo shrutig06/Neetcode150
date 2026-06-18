425f302b4736d1e67cdf3579d00d1788930d6c76
Shruti Gupta <shrutigupta72552@gmail.com>
2026-06-18 23:21:23 +0530

    Time: 26 ms (62.94%) | Memory: 18.8 MB (37.01%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,10 +1,28 @@
 class Solution {
 public:
+    queue<pair<int, int>> q;
+    vector<int> move = {-1, 0, 1, 0};
+
+    void runBFS(const vector<vector<char>>& grid, vector<vector<bool>>& vis) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        while (!q.empty()) {
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            for (int i = 0; i <= 3; i++) {
+                int newX = x + move[i], newY = y + move[3 - i];
+                if (newX >= 0 && newY >= 0 && newX < rows && newY < cols &&
+                    !vis[newX][newY] && grid[newX][newY] == '1') {
+                    q.push({newX, newY});
+                    vis[newX][newY] = true;
+                }
+            }
+        }
+    }
     int numIslands(vector<vector<char>>& grid) {
         int rows = grid.size();
         int cols = grid[0].size();
-        queue<pair<int, int>> q;
-        vector<int> move = {-1, 0, 1, 0};
         vector<vector<bool>> vis(rows, vector<bool>(cols, false));
         int cnt = 0;
 
@@ -12,23 +30,9 @@ public:
             for (int j = 0; j < cols; j++) {
                 if (!vis[i][j] && grid[i][j] == '1') {
                     q.push({i, j});
-                    vis[i][j]=true;
+                    vis[i][j] = true;
                     cnt++;
-                }
-              
-                while (!q.empty()) {
-                    int x = q.front().first;
-                    int y = q.front().second;
-                    q.pop();
-                    for (int i = 0; i <= 3; i++) {
-                        int newX = x + move[i], newY = y + move[3 - i];
-                        if (newX >= 0 && newY >= 0 && newX < rows &&
-                            newY < cols && !vis[newX][newY] &&
-                            grid[newX][newY] == '1') {
-                            q.push({newX, newY});
-                            vis[newX][newY] = true;
-                        }
-                    }
+                    runBFS(grid, vis);
                 }
             }
         }
