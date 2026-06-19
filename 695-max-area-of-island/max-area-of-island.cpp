d0e473c2d1bd152691747b5d7776997f5ad3f3db
Shruti Gupta <shrutigupta72552@gmail.com>
2026-06-19 10:29:43 +0530

    Time: 4 ms (39.23%) | Memory: 27.6 MB (68.88%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    void runDFS(vector<vector<int>>& grid, int x, int y, int& rows, int& cols, int& area){
-        grid[x][y]=2;
+    void runDFS(vector<vector<int>>& grid, int x, int y, int& area) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        if (x < 0 || y < 0 || x >= rows || y >= cols || grid[x][y] != 1)
+            return;
+        grid[x][y] = 2;
         area++;
-        if(x>=0 && x<rows && y-1>=0 && y<cols && grid[x][y-1]==1){
-            runDFS(grid, x, y-1, rows, cols, area);
-        }
-        if(x>=0 && x<rows && y>=0 && y+1<cols && grid[x][y+1]==1){
-            runDFS(grid, x, y+1, rows, cols, area);
-        }
-        if(x-1>=0 && x<rows && y>=0 && y<cols && grid[x-1][y]==1){
-            runDFS(grid, x-1, y, rows, cols, area);
-        }
-        if(x+1<rows && x>=0 && y>=0 && y<cols && grid[x+1][y]==1){
-            runDFS(grid, x+1, y, rows, cols, area);
-        }
+        runDFS(grid, x, y - 1, area);
+        runDFS(grid, x, y + 1, area);
+        runDFS(grid, x - 1, y, area);
+        runDFS(grid, x + 1, y, area);
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int rows=grid.size();
-        int cols=grid[0].size();
-        int maxArea=0;
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(grid[i][j]==1){
-                    int area=0;
-                    runDFS(grid, i, j, rows, cols, area);
-                    maxArea=max(maxArea, area);
+        int rows = grid.size();
+        int cols = grid[0].size();
+        int maxArea = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    int area = 0;
+                    runDFS(grid, i, j, area);
+                    maxArea = max(maxArea, area);
                 }
             }
         }
