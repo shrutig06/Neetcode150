477f75d6ab5e0a2fdba17c80069a941a155e2c6b
Shruti Gupta <shrutigupta72552@gmail.com>
2026-07-10 09:20:52 +0530

    Time: 80 ms (59.20%) | Memory: 89.2 MB (14.77%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -4,13 +4,12 @@ public:
         unordered_set<int> s(nums.begin(), nums.end());
         int cnt = 0;
         int maxCnt = 0;
-        for (auto x:s) {
-            if (s.find(x - 1)==s.end()) {
-                int curr = x;
+        for (auto x : s) {
+            if (s.find(x - 1) == s.end()) {
                 cnt = 1;
-                while (s.count(curr + 1)) {
+                while (s.find(x + 1) != s.end()) {
                     cnt++;
-                    curr++;
+                    x++;
                 }
             }
             maxCnt = max(maxCnt, cnt);
