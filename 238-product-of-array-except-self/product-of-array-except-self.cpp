8d9cd9efe95debac472206bfb608d0df0e3bb03c
Shruti Gupta <shrutigupta72552@gmail.com>
2026-07-10 09:50:17 +0530

    Time: 6 ms (6.88%) | Memory: 42.3 MB (17.47%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -5,15 +5,14 @@ public:
         if(n==1) return nums;
         vector<int> prefix(n), suffix(n);
         prefix[0]=1; suffix[n-1]=1;
-        vector<int> res(n);
         for(int i=1;i<n;i++){
             prefix[i]=prefix[i-1]*nums[i-1];
             suffix[n-i-1]=suffix[n-i]*nums[n-i];
         }
 
         for(int i=0;i<n;i++){
-            res[i]=prefix[i]*suffix[i];
+            nums[i]=prefix[i]*suffix[i];
         }
-        return res;
+        return nums;
     }
 };
