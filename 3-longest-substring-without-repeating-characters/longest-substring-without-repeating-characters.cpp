dd91ff57fdeae345f7b1f9a28218ce862ec27aef
Shruti Gupta <shrutigupta72552@gmail.com>
2026-07-12 12:05:51 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (95.95%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -5,22 +5,14 @@ public:
         vector<bool> vis(100, false);
         int maxCnt = 0;
         while (right < s.length()) {
-            if (!vis[s[right]]) {
-                vis[s[right]] = true;
-                maxCnt = max(maxCnt, right - left + 1);
-                right++;
-            } else if (vis[s[right]]) {
-                while (left < right) {
-                    if (vis[s[left]]) {
-                        vis[s[left]] = false;
-                        left++;
-                        break;
-                    } else {
-                        vis[s[left]] = false;
-                        left++;
-                    }
-                }
+            while(vis[s[right]]){
+                vis[s[left]]=false;
+                left++;
             }
+
+            vis[s[right]]=true;
+            maxCnt=max(maxCnt, right-left+1);
+            right++;
         }
 
         return maxCnt;
