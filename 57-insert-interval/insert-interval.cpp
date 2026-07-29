df3c509ebdde13db59c5d406952cc0f1411ab57b
Shruti Gupta <shrutigupta72552@gmail.com>
2026-07-29 10:53:36 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (76.85%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals,
-                               vector<int>& newInterval) {
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> res;
-        intervals.push_back(newInterval);
-        sort(intervals.begin(), intervals.end());
-        res.push_back(intervals[0]);
+        int i=0;
 
-        for (int i = 1; i < intervals.size(); i++) {
-            if(res.back()[1]>=intervals[i][0]){
-                res.back()[1]=max(res.back()[1], intervals[i][1]);
-            } else{
-                res.push_back(intervals[i]);
-            }
+        while(i<intervals.size() && newInterval[0]>intervals[i][1]){
+            res.push_back(intervals[i]);
+            i++;
         }
+
+        while(i<intervals.size() && newInterval[0]<=intervals[i][1] && newInterval[1]>=intervals[i][0]){
+            newInterval[0]=min(newInterval[0], intervals[i][0]);
+            newInterval[1]=max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        res.push_back(newInterval);
+
+        while(i<intervals.size()){
+            res.push_back(intervals[i]);
+            i++;
+        }
+
         return res;
     }
 };
