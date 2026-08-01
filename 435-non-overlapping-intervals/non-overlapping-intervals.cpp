e97bf5db81260296e363cee56f814349200cd559
Shruti Gupta <shrutigupta72552@gmail.com>
2026-08-01 15:03:53 +0530

    Time: 58 ms (38.07%) | Memory: 94 MB (39.64%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -8,8 +8,8 @@ public:
         for(int i=1;i<intervals.size();i++){
             if(intervals[i][0]>=lastKept[1]){
                 lastKept=intervals[i];
-            } else {
-                lastKept[1]=min(intervals[i][1],lastKept[1]);
+            } else if(lastKept[1]>intervals[i][0]){
+                if(lastKept[1]>intervals[i][1]) lastKept=intervals[i];
                 cnt++;
             }
         }
