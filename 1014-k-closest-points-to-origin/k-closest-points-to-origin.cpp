ac9096c3e75aa8173261783ed02ee274c9496c5f
Shruti Gupta <shrutigupta72552@gmail.com>
2026-08-04 15:02:11 +0530

    Time: 119 ms (8.99%) | Memory: 87.2 MB (8.81%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    struct comp {
+        bool operator()(const pair<vector<int>, double>& p1,
+                        pair<vector<int>, double>& p2) const {
+            return p1.second < p2.second;
+        }
+    };
+
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<vector<int>, double>, vector<pair<vector<int>, double>>,
+                       comp>
+            maxHeap;
+        for (auto point : points) {
+            int x = point[0], y = point[1];
+            double dist = sqrt(x * x + y * y);
+            maxHeap.push({{x, y}, dist});
+            if (maxHeap.size() > k)
+                maxHeap.pop();
+        }
+
+        vector<vector<int>> ans;
+        while (!maxHeap.empty()) {
+            auto val = maxHeap.top();
+            ans.push_back({val.first[0], val.first[1]});
+            maxHeap.pop();
+        }
+
+        return ans;
+    }
+};
