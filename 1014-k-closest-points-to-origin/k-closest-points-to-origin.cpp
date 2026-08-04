b35318b647bf898bcdd940bc894cfecb731390bf
Shruti Gupta <shrutigupta72552@gmail.com>
2026-08-04 15:37:13 +0530

    Time: 113 ms (10.74%) | Memory: 87.1 MB (9.47%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
-    struct comp {
-        bool operator()(const pair<vector<int>, double>& p1,
-                        pair<vector<int>, double>& p2) const {
-            return p1.second < p2.second;
-        }
-    };
-
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<vector<int>, double>, vector<pair<vector<int>, double>>,
-                       comp>
-            maxHeap;
+        //{distance, point} priority queue is by default max heap
+        priority_queue<pair<int,vector<int>>> maxHeap;
         for (auto point : points) {
             int x = point[0], y = point[1];
-            double dist = x * x + y * y;
-            maxHeap.push({{x, y}, dist});
+            int dist = x * x + y * y;
+            maxHeap.push({dist, {x,y}});
             if (maxHeap.size() > k)
                 maxHeap.pop();
         }
@@ -22,7 +14,7 @@ public:
         vector<vector<int>> ans;
         while (!maxHeap.empty()) {
             auto val = maxHeap.top();
-            ans.push_back({val.first[0], val.first[1]});
+            ans.push_back({val.second[0], val.second[1]});
             maxHeap.pop();
         }
 
