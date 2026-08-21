849da1b569e871deb0ba4d849f9c7f7f1ac86c17
Shruti Gupta <shrutigupta72552@gmail.com>
2026-08-21 16:52:37 +0530

    Time: 3 ms (95.67%) | Memory: 12.5 MB (23.22%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -10,7 +10,9 @@ class Solution {
 public:
     void deleteNode(ListNode* node) {
         // Approach => When I cannot modify the previous node, can I modify the current node to absorb the next node and then bypass the next node?
+        ListNode* del=node->next;
         node->val = node->next->val;
         node->next = node->next->next;
+        delete(del);
     }
 };
