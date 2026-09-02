3a32d5f98e4387e0a25fd97b7743bbf596e0cc3d
Shruti Gupta <shrutigupta72552@gmail.com>
2026-09-02 19:16:29 +0530

    Time: 54 ms (14.31%) | Memory: 69.1 MB (83.20%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,14 +1,38 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> minHeap;
-        
-        //TC = O(NlogK)Space Complexity O(K)
-        for(int i=0;i<nums.size();i++){
-            minHeap.push(nums[i]);
-            if(minHeap.size()>k) minHeap.pop();
+ int partition_algo(vector<int>& nums, int L, int R){
+    int i=L+1, j=R;
+
+    int pivot=L;
+
+    while(i<=j){
+        if(nums[i]<nums[pivot] && nums[j]>nums[pivot]){
+            swap(nums[i], nums[j]);
+            i++; j--;
         }
 
-        return minHeap.top();
+        if(nums[i]>=nums[pivot]) i++;
+        if(nums[j]<=nums[pivot]) j--;
+    }
+    swap(nums[j], nums[pivot]);
+    return j;
+ }
+    int findKthLargest(vector<int>& nums, int k) {
+        int n = nums.size();
+        int L = 0, R = n - 1;
+        int pivot_idx=0;
+        while(true){
+            pivot_idx=partition_algo(nums, L, R);
+
+            if(pivot_idx==k-1){
+               break;
+            }else if(pivot_idx>k-1){
+                R=pivot_idx-1;
+            }else{
+                L=pivot_idx+1;
+            }
+        }
+
+        return nums[pivot_idx];
     }
 };
